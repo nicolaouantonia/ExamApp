6c6086e7f45082598353f342607a28440d239a82
Antonia Nikolaou <antonini@amdocs.com>
2022-09-09 14:19:30 +0000

    Update src/test/java/com/agiletestingalliance/AboutCPDOFTest.java

--- src/test/java/com/agiletestingalliance/AboutCPDOFTest.java
+++ src/test/java/com/agiletestingalliance/AboutCPDOFTest.java
@@ -10,7 +10,7 @@ public class AboutCPDOFTest {
 	@Test
 	public void testDesc() throws Exception {
 		String result = new AboutCPDOF().desc();
-        assertTrue("AboutCPDOF",result.contains("CP-DOF is designed specifically for corporates and working professionals alike."));
+        assertTrue("AboutCPDOF",result.contains("CP-DOF certification program covers end to end DevOps Life Cycle practically."));
 
 	}
 
