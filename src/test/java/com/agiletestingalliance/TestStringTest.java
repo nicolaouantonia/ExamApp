bbf3363e0c051415391821f3d0d3ed647427ced7
Antonia Nikolaou <antonini@amdocs.com>
2022-09-09 14:05:47 +0000

    Update src/test/java/com/agiletestingalliance/TestStringTest.java, src/test/java/com/agiletestingalliance/UsefulnessTest.java

---
+++ src/test/java/com/agiletestingalliance/TestStringTest.java
@@ -0,0 +1,17 @@
+package com.agiletestingalliance;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TestStringTest {
+
+	@Test
+	public void testDesc() throws Exception {
+        // TestString
+		// String result = new TestString().gstr();
+        // assertTrue("TestString",result.contains("String value ="));
+
+	}
+
+}
