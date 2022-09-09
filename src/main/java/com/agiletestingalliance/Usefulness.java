fc43ab6f6aec24be4805c23e48a64949ae302e7a
Antonia Nikolaou <antonini@amdocs.com>
2022-09-09 13:25:06 +0000

    Update src/main/java/com/agiletestingalliance/Usefulness.java

--- src/main/java/com/agiletestingalliance/Usefulness.java
+++ src/main/java/com/agiletestingalliance/Usefulness.java
@@ -3,9 +3,8 @@ package com.agiletestingalliance;
 public class Usefulness{
 
 	  public String desc() {
-  		  String str = "DevOps is about transformation, about building quality in, improving productivity and about automation in Dev, Testing and Operations. <br> <br> CP-DOF is a one of its kind initiative to marry 2 distinct worlds of Agile and Operations together. <br> <br> <b>CP-DOF </b> helps you learn DevOps fundamentals along with Continuous Integration and Continuous Delivery and deep dive into DevOps concepts and mindset.";
+  		  return "DevOps is about transformation, about building quality in, improving productivity and about automation in Dev, Testing and Operations. <br> <br> CP-DOF is a one of its kind initiative to marry 2 distinct worlds of Agile and Operations together. <br> <br> <b>CP-DOF </b> helps you learn DevOps fundamentals along with Continuous Integration and Continuous Delivery and deep dive into DevOps concepts and mindset.";
 
-		  return str;
 
   	}
 
