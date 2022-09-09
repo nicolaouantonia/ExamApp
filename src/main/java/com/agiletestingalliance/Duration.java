ca717855495cfdae17561e665cc6780cade30826
Antonia Nikolaou <antonini@amdocs.com>
2022-09-09 13:22:28 +0000

    Update src/main/java/com/agiletestingalliance/Duration.java

--- src/main/java/com/agiletestingalliance/Duration.java
+++ src/main/java/com/agiletestingalliance/Duration.java
@@ -3,8 +3,7 @@ package com.agiletestingalliance;
 public class Duration{
 
   public String dur() {
-    String str = "CP-DOF is designed specifically for corporates and working professionals alike. If you are a corporate and can't dedicate full day for training, then you can opt for either half days course or  full days programs which is followed by theory and practical exams.";
-    return str;
+    return "CP-DOF is designed specifically for corporates and working professionals alike. If you are a corporate and can't dedicate full day for training, then you can opt for either half days course or  full days programs which is followed by theory and practical exams.";
 
   }
 
