a237fad7ab1c171d8b8b6f51dec93749cbd0dc1b
Antonia Nikolaou <antonini@amdocs.com>
2022-09-09 14:37:44 +0000

    Update src/test/java/com/agiletestingalliance/MinMaxTest.java

--- src/test/java/com/agiletestingalliance/MinMaxTest.java
+++ src/test/java/com/agiletestingalliance/MinMaxTest.java
@@ -15,9 +15,23 @@ public class MinMaxTest {
 	}
 
 	@Test
+	public void testFoo() throws Exception {
+		int result = new MinMax().foo(4,3);
+       assertEquals("foo", result, 4);
+
+	}
+
+	@Test
 	public void testBar() throws Exception {
 	  String result = new MinMax().bar("an");
        assertEquals("Bar", result, "an");
+
+	}
+
+	@Test
+	public void testBar() throws Exception {
+	  String result = new MinMax().bar("");
+       assertEquals("Bar empty", result, "");
 
 	}
 
