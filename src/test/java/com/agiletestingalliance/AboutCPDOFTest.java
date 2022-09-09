4bba218ba4319e843bbe9fbcffe546e2fe30a208
Antonia Nikolaou <antonini@amdocs.com>
2022-09-09 14:16:40 +0000

    Update src/test/java/com/agiletestingalliance/AboutCPDOFTest.java

--- src/test/java/com/agiletestingalliance/AboutCPDOFTest.java
+++ src/test/java/com/agiletestingalliance/AboutCPDOFTest.java
@@ -9,7 +9,7 @@ public class AboutCPDOFTest {
 
 	@Test
 	public void testDesc() throws Exception {
-		String result = new AboutCPDOF().dur();
+		String result = new AboutCPDOF().desc();
         assertTrue("AboutCPDOF",result.contains("CP-DOF is designed specifically for corporates and working professionals alike."));
 
 	}
