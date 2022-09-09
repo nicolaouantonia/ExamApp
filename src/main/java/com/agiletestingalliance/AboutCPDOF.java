56c17a9b8d361cc5ffa7360b415fd3723b77bae7
Antonia Nikolaou <antonini@amdocs.com>
2022-09-09 13:13:04 +0000

    AboutCPDOF

--- src/main/java/com/agiletestingalliance/AboutCPDOF.java
+++ src/main/java/com/agiletestingalliance/AboutCPDOF.java
@@ -3,8 +3,7 @@ package com.agiletestingalliance;
 public class AboutCPDOF{
 
   public String desc() {
-    String str = "CP-DOF certification program covers end to end DevOps Life Cycle practically. CP-DOF is the only globally recognized certification program which has the following key advantages: <br> 1. Completely hands on. <br> 2. 100% Lab/Tools Driven <br> 3. Covers all the tools in entire lifecycle <br> 4. You will not only learn but experience the entire DevOps lifecycle. <br> 5. Practical Assessment to help you solidify your learnings.";
-    return str;
+    return "CP-DOF certification program covers end to end DevOps Life Cycle practically. CP-DOF is the only globally recognized certification program which has the following key advantages: <br> 1. Completely hands on. <br> 2. 100% Lab/Tools Driven <br> 3. Covers all the tools in entire lifecycle <br> 4. You will not only learn but experience the entire DevOps lifecycle. <br> 5. Practical Assessment to help you solidify your learnings.";
   
   }
   
