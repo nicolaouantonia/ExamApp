d8b691373dd7afb7b24bb3d7b39e22899dd9709f
Antonia Nikolaou <antonini@amdocs.com>
2022-09-09 13:41:50 +0000

    Update /src/test/java/com/agiletestingalliance/CPDOFSEPFOURIntegrationTest.java

--- src/test/java/com/agiletestingalliance/CPDOFSEPFOURIntegrationTest.java
+++ src/test/java/com/agiletestingalliance/CPDOFSEPFOURIntegrationTest.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.net.URL;
 import org.apache.commons.io.FileUtils;
 
-public class CounterWebAppIntegrationTest {
+public class CPDOFSEPFOURIntegrationTest {
 
     WebDriver driver;
     String hubURL = null;
