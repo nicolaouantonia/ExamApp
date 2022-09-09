204913b0a499370d9f41f3de25ca904d6d046f0a
Antonia Nikolaou <antonini@amdocs.com>
2022-09-09 13:50:52 +0000

    Update src/test/java/com/agiletestingalliance/CPDOFIntegrationTest.java

--- src/test/java/com/agiletestingalliance/CPDOFIntegrationTest.java
+++ src/test/java/com/agiletestingalliance/CPDOFIntegrationTest.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.net.URL;
 import org.apache.commons.io.FileUtils;
 
-public class CPDOFSEPFOURIntegrationTest {
+public class CPDOFIntegrationTest {
 
     WebDriver driver;
     String hubURL = null;
